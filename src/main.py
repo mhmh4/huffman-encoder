f6ec8b8af18e189d5bb295d56381bec0b4797df2
u06 <u06_m@protonmail.com>
2021-08-17 08:22:47 -0400

    finalized

--- src/main.py
+++ src/main.py
@@ -23,7 +23,10 @@ def ascii_string_to_binary(s: str) -> str:
 def create_results_file(results: list) -> None:
     with open("output/results.txt", "w") as f:
         for r in results:
-            f.write(f"{r[0]}: {r[1]}\n")
+            if r:
+                f.write(f"{r[0]}: {r[1]}\n")
+            else:
+                f.write("\n")
 
 
 def main():
@@ -40,22 +43,37 @@ def main():
     with open(path, "r") as f:
         message = f.read()
 
+    binary_original = ascii_string_to_binary(message)
+    original_num_bits = len(binary_original)
+
     frequency = character_frequency(path)
     ht = HuffmanTree(frequency)
-    # print(f"{message}", type(message))
-    binary = ascii_string_to_binary(message)
-    binary_huffman = ht.encode(message)
 
+    binary_huffman, table = ht.encode(message)
+    new_num_bits = len(binary_huffman)
+    num_bits_in_table = len("".join(v[1] for v in table.values()))
+    print(num_bits_in_table)
+    table_size = (len(table)*8) + num_bits_in_table
+    total_bit_size = table_size + new_num_bits
+
+    compression_ratio = total_bit_size / original_num_bits
+
+    # to make the output file easier to read, `None` will be used to
+    # write new line characters
     results = [
         ("original message", message),
-        ("message in binary", binary),
-        ("message bit size", len(binary)),
-        ("message byte size", len(binary) // 8),
-        ("message in binary using huffman coding", binary_huffman)
+        ("message in binary", binary_original),
+        ("message bit size", original_num_bits),
+        None,
+        ("message in binary using huffman coding", binary_huffman),
+        ("new bit size", new_num_bits),
+        ("table size", table_size),
+        ("total size", total_bit_size),
+        None,
+        ("compression results", f"{compression_ratio:0.2%} compression")
     ]
 
     create_results_file(results)
-
 
 
 if __name__ == "__main__":
